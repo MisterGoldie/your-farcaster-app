6441733caeff937f96f508a6d3338deaaf911035
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-10 18:28:46 -0500

    regdben

--- src/app/howtoplay/page.tsx
+++ src/app/howtoplay/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function HowToPlay() {
   return (
     <main className="p-4 flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-4 text-center">How to Play Tic-Tac-Toe</h1>
+      <h1 className="text-4xl font-bold mb-4 text-center" style={{ fontFamily: "'Butcherman', cursive" }}>How to Play Tic-Tac-Toe</h1>
       <div className="flex flex-col items-center">
         <Image 
           src="https://bafybeifzk7uojcicnh6yhnqvoldkpzuf32sullm34ela266xthbidca6ny.ipfs.w3s.link/HowToPlay%20(1).png" 
@@ -12,7 +12,7 @@ export default function HowToPlay() {
           width={500}
           height={500}
         />
-        <Link href="/game" className="bg-purple-500 text-white px-6 py-3 rounded mt-8 text-center text-lg hover:bg-purple-600 transition-colors">
+        <Link href="/game" className="bg-purple-500 text-white px-6 py-3 rounded mt-8 text-center text-lg hover:bg-purple-600 transition-colors" style={{ fontFamily: "'Butcherman', cursive" }}>
           Start Game
         </Link>
       </div>
