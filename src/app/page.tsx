231c1449c6eb289e053d1daa2b0c56fc20beb838
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-10 18:20:48 -0500

    fsbrrw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="p-4 flex flex-col items-center" style={{ fontFamily: "'Rubik Wet Paint', cursive" }}>
-      <p className="my-4 text-center text-xl">
+    <main className="p-4 flex flex-col items-center">
+      <p className="my-4 text-center text-xl" style={{ fontFamily: "'Rock Salt', cursive" }}>
         Welcome to POD Play presented by /thepod 🕹️
       </p>
       
@@ -17,7 +17,7 @@ export default function Home() {
         />
       </div>
       
-      <Link href="/howtoplay" className="bg-purple-500 text-white px-6 py-3 rounded mt-8 text-center text-lg hover:bg-purple-600 transition-colors">
+      <Link href="/howtoplay" className="bg-purple-500 text-white px-6 py-3 rounded mt-8 text-center text-lg hover:bg-purple-600 transition-colors" style={{ fontFamily: "'Rock Salt', cursive" }}>
         Start
       </Link>
     </main>
