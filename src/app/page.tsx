f4a84263e2ed775d4942ea752b33d87d0a22c220
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-10 14:37:23 -0500

    dsvsbs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="p-4 flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-4 text-center">Welcome to POD Play Tic-Tac-Toe</h1>
+      <h1 className="text-4xl font-bold mb-4 text-center">Welcome to POD Play</h1>
       <div style={{ width: '500px', height: '500px', position: 'relative' }}>
         <Image 
           src="https://bafybeidnv5uh2ne54dlzyummobyv3bmc7uzuyt5htodvy27toqqhijf4xu.ipfs.w3s.link/PodPlay.gif" 
@@ -13,7 +13,7 @@ export default function Home() {
           objectFit="contain"
         />
       </div>
-      <p className="my-4 text-center">Welcome to POD Play presented by /thepod 🕹️. Think you can win a game of Tic-Tac-Toe?</p>
+      <p className="my-4 text-center">Welcome to POD Play presented by /thepod 🕹️</p>
       <Link href="/howtoplay" className="bg-blue-500 text-white px-4 py-2 rounded text-center">
         Start
       </Link>
