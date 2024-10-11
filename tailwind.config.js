839cef0d3dbd7334371ba70c8f1cdc2d6930abb5
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-11 11:53:13 -0500

    JHCHJVK

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        frijole: ["Frijole", "system-ui"],
+      },
+    },
   },
   plugins: [],
 }
