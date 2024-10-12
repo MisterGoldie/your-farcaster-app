95528e96602a13794723e9fde9c0254cc9d53597
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-11 20:37:46 -0500

    evwvevsbe

--- src/app/compose/route.ts
+++ src/app/compose/route.ts
@@ -1,44 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type ComposerActionFormResponse = {
-  type: 'form';
-  title: string;
-  url: string;
-};
-
-type ComposerActionMetadata = {
-  type: 'composer';
-  name: string;
-  icon: string;
-  description: string;
-  aboutUrl: string;
-  imageUrl: string;
-  action: {
-    type: string;
-  };
-};
-
-export async function GET(req: NextRequest) {
-  return NextResponse.json({
-    "type": "composer",
-    "name": "POD Play Tic-Tac-Toe",
-    "icon": "gamepad",
-    "description": "Play Tic-Tac-Toe presented by /thepod",
-    "aboutUrl": "https://your-farcaster-app.vercel.app/about",
-    "imageUrl": "https://bafybeidnv5uh2ne54dlzyummobyv3bmc7uzuyt5htodvy27toqqhijf4xu.ipfs.w3s.link/PodPlay.gif",
-    "action": {
-      "type": "post",
-    }
-  } as ComposerActionMetadata);
-}
-
-export async function POST(req: NextRequest) {
-  const data = await req.json();
-  console.log(data);
-
-  return NextResponse.json({ 
-    type: 'form',
-    title: 'POD Play Tic-Tac-Toe',
-    url: 'https://your-farcaster-app.vercel.app', // Replace with your actual deployed app URL
-  } as ComposerActionFormResponse);
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    res.status(200).json({
+      "type": "composer",
+      "name": "POD Play Tic-Tac-Toe",
+      "icon": "joystick",
+      "description": "View POD Play Tic-Tac-Toe within Farcaster",
+      "aboutUrl": "https://your-farcaster-app.vercel.app/",
+      "imageUrl": "https://bafybeidnv5uh2ne54dlzyummobyv3bmc7uzuyt5htodvy27toqqhijf4xu.ipfs.w3s.link/PodPlay.gif",
+      "action": {
+        "type": "web",
+        "url": "https://your-farcaster-app.vercel.app"
+      }
+    });
+  } else if (req.method === 'POST') {
+    res.status(200).json({
+      type: 'web',
+      url: 'https://your-farcaster-app.vercel.app',
+    });
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
 }
