c0138608fd7e1d2612bb1a18683790c67efa8b0a
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-11 21:13:21 -0500

    rwgsrgdxb

---
+++ compose.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+type ComposerActionFormResponse = {
+  type: 'form';
+  title: string;
+  url: string;
+};
+
+type ComposerActionMetadata = {
+  type: 'composer';
+  name: string;
+  icon: string;
+  description: string;
+  aboutUrl: string;
+  imageUrl: string;
+  action: {
+    type: string;
+  };
+};
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  console.log(data);
+
+  return NextResponse.json({ 
+    type: 'form',
+    title: 'POD Play Tic-Tac-Toe',
+    url: 'https://your-farcaster-app.vercel.app',
+  } as ComposerActionFormResponse);
+}
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json({
+    "type": "composer",
+    "name": "POD Play Tic-Tac-Toe",
+    "icon": "joystick",
+    "description": "View POD Play Tic-Tac-Toe within Farcaster",
+    "aboutUrl": "https://your-farcaster-app.vercel.app/",
+    "imageUrl": "https://bafybeidnv5uh2ne54dlzyummobyv3bmc7uzuyt5htodvy27toqqhijf4xu.ipfs.w3s.link/PodPlay.gif",
+    "action": {
+      "type": "web",
+      "url": "https://your-farcaster-app.vercel.app"
+    }
+  } as ComposerActionMetadata);
+}
