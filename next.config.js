33c3aa52902a80bdbd030b698a8d1ab4ff80fb0a
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-12 23:35:03 -0500

    dsfargwsg

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; connect-src 'self' https://* wss://ws-us3.pusher.com blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live blob:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://vercel.live; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: https://*.ipfs.w3s.link blob:; frame-src 'self' https://vercel.live; worker-src blob: 'self'; child-src blob:;"
+            value: "default-src 'self'; connect-src 'self' https://* wss://ws-us3.pusher.com blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live blob:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://vercel.live; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob:; frame-src 'self' https://vercel.live; worker-src blob: 'self'; child-src blob:;"
           },
         ],
       },
@@ -30,8 +30,8 @@ const nextConfig = {
           outputPath: 'static/images/',
         },
       },
-    })
-    return config
+    });
+    return config;
   },
 }
 
