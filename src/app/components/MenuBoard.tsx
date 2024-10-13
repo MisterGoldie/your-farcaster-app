567e420b0ef6f7e7d56fa8c209a15687bb23236e
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-12 23:58:20 -0500

    dfxbsfdbfsb

--- src/app/components/MenuBoard.tsx
+++ src/app/components/MenuBoard.tsx
@@ -1,9 +1,35 @@
 import React, { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, ThreeEvent } from '@react-three/fiber'
 import { Text } from '@react-three/drei'
 import * as THREE from 'three'
 
-function Board() {
+type MenuOptionProps = {
+  position: [number, number, number]
+  text: string
+  onClick: () => void
+}
+
+function MenuOption({ position, text, onClick }: MenuOptionProps) {
+  return (
+    <group position={position}>
+      <mesh onClick={onClick}>
+        <planeGeometry args={[2, 0.5]} />
+        <meshStandardMaterial color="#ff6600" />
+      </mesh>
+      <Text
+        position={[0, 0, 0.01]}
+        fontSize={0.2}
+        color="#000000"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {text}
+      </Text>
+    </group>
+  )
+}
+
+function Board({ onStartGame, onGoBack }: { onStartGame: () => void, onGoBack: () => void }) {
   const boardRef = useRef<THREE.Group>(null)
 
   useFrame(() => {
@@ -14,15 +40,21 @@ function Board() {
 
   return (
     <group ref={boardRef} scale={[1, 1, 1]}>
-      <Text
+      <MenuOption
         position={[0, 0, 0]}
-        fontSize={0.5}
-        color="#000000"
-        anchorX="center"
-        anchorY="middle"
-      >
-        Menu
-      </Text>
+        text="Menu"
+        onClick={() => console.log('Menu clicked')}
+      />
+      <MenuOption
+        position={[0, 0, -1]}
+        text="Start Game"
+        onClick={onStartGame}
+      />
+      <MenuOption
+        position={[0, 0, -2]}
+        text="Go Back"
+        onClick={onGoBack}
+      />
     </group>
   )
 }
@@ -34,10 +66,9 @@ export default function MenuBoard() {
         <Canvas>
           <ambientLight intensity={0.5} />
           <pointLight position={[10, 10, 10]} />
-          <Board />
+          <Board onStartGame={() => console.log('Start game')} onGoBack={() => console.log('Go back')} />
         </Canvas>
       </div>
     </div>
   )
 }
-
--- src/app/howtoplay/page.tsx
+++ src/app/howtoplay/page.tsx
@@ -3,32 +3,29 @@
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 
-const MenuBoard = dynamic(() => import('../components/MenuBoard'), { ssr: false })
+// Define the props type for MenuBoard
+type MenuBoardProps = {
+  onStartGame: () => void
+  onGoBack: () => void
+}
+
+// Use the defined type in the dynamic import
+const MenuBoard = dynamic<MenuBoardProps>(() => import('../components/MenuBoard'), { ssr: false })
 
 export default function HowToPlay() {
   const router = useRouter()
 
-  const handleBackToHome = () => {
+  const handleStartGame = () => {
+    router.push('/game')
+  }
+
+  const handleGoBack = () => {
     router.push('/')
   }
 
   return (
     <main className="h-[100svh] bg-black text-white overflow-hidden">
-      <MenuBoard />
-      <div className="absolute top-3 left-2 z-10"> 
-        <button onClick={handleBackToHome}>
-          <svg
-            className="w-6 h-6"
-            xmlns="http://www.w3.org/2000/svg"
-            height="26"
-            viewBox="0 -960 960 960"
-            width="26"
-            fill="#FFFFFF"
-          >
-            <path d="M480-160 160-480l320-320 42 42-248 248h526v60H274l248 248-42 42Z"/>
-          </svg>
-        </button>
-      </div>
+      <MenuBoard onStartGame={handleStartGame} onGoBack={handleGoBack} />
     </main>
   )
 }
