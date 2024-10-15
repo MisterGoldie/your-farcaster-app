49f700bcc0642e10cbf679f9bbe2a89a8de7a2fd
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-15 11:17:24 -0500

    dsvdvdx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
-import { metadata } from './metadata'
+import './globals.css'
+import { Metadata } from 'next'
 
-export { metadata }
+export const metadata: Metadata = {
+  title: 'POD Play Tic-Tac-Toe',
+  description: 'A Tic-Tac-Toe game presented by /thepod',
+}
 
 export default function RootLayout({
   children,
@@ -9,7 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Frijole&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+      </head>
+      <body className="bg-black flex items-center justify-center min-h-screen">
+        <div className="w-full max-w-md px-4">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
