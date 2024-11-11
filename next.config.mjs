41d20dfb95d6bae11f938425a7d97170e8e26b0c
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-11-10 21:02:12 -0600

    gdvsfsfg

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Add any other Next.js config options here
-}
-
-export default nextConfig
--- src/types/global.d.ts
+++
@@ -1,8 +0,0 @@
-// /// <reference types="firebase" />
-
-// src/types/farcaster-auth-client.d.ts
-declare module '@farcaster/auth-client' {
-    export function createAppClient(config: any): any;
-    export function viemConnector(): any;
-    // Add other necessary type declarations based on the package's API
-}
